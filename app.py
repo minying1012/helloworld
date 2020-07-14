ef89b4c911cdfcbb72480c1e07027636b6cb84e9
minying1012 <jmy8361@gmail.com>
2020-07-15 00:26:43 +0900

    test3

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def postSurveyData():
     return jsonify(data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 443))
+    port = int(os.environ.get("PORT", 80))
     app.run(debug=True, host='0.0.0.0', port=port)
