c8ded6126f8f768f4519e4be088518fc2d037bbd
minying1012 <jmy8361@gmail.com>
2020-07-15 14:27:55 +0900

    Apply working POST Api

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask, render_template, jsonify, request
+from flask_restful import Resource, reqparse, Api
 import os
 
+class postSurveyData(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=str, required=True)
+    parser.add_argument('surveyResult', type=list, location='json', required=True)
+    parser.add_argument('location', type=str, required=True)
+
+    def post(self):
+        try:
+            args = postSurveyData.parser.parse_args()
+            surveryList = args['surveyResult']
+            surveyYesSum = 0
+            for item in surveryList:
+                if item == 1:
+                    surveyYesSum += 1
+                elif isinstance(item, int) == False:
+                    return {'error': 'surveyResult only allowed integer type'}, 400
+                elif item != 0 & item != 1:
+                    return {'error': 'surveyResult allowed only 1 and 0 value'}, 400
+
+            percent = surveyYesSum/len(surveryList)*100
+            
+            return {'precent': str(int(percent))}, 200
+        except Exception as e:
+            return {'error': str(e)}
+
 app = Flask(__name__)
+api = Api(app)
+api.add_resource(postSurveyData, '/api/postSurveyData')
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-@app.route('/api', methods=['POST'])
-def postSurveyData():
-    data = request.get_json()
-    #return render_template('test11.html')
-    return jsonify(data)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 80))
